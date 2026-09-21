cf258c69ae5dbb0331e6348b14f111853c6c613c
Murugeshwari K <murugu487@gmail.com>
2026-09-21 21:08:18 +0530

    Delete hello.java

--- hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class hello{
-    public static void main(String[] args){
-        Scanner mur = new Scanner(System.in);
-        int a = mur.nextInt();
-        int b = mur.nextInt();
-        System.out.print(a+b);
-    }
-}
